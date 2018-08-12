83d6f6a45a5da11acf1a0f35cf82f8817a8df74e
Dmitrii Goriunov <goriunovd@gmail.com>
2018-08-12 20:20:40 +1200

    Update dep

--- make.bat
+++ make.bat
@@ -2,17 +2,17 @@ call "%VS140COMNTOOLS%..\..\vc\vcvarsall.bat" amd64
 
 if not exist targets (
   mkdir targets
-  curl https://nodejs.org/dist/v8.1.2/node-v8.1.2-headers.tar.gz | tar xz -C targets
-  curl https://nodejs.org/dist/v8.1.2/win-x64/node.lib > targets/node-v8.1.2/node.lib
-  curl https://nodejs.org/dist/v9.2.0/node-v9.2.0-headers.tar.gz | tar xz -C targets
-  curl https://nodejs.org/dist/v9.2.0/win-x64/node.lib > targets/node-v9.2.0/node.lib
-  curl https://nodejs.org/dist/v10.0.0/node-v10.0.0-headers.tar.gz | tar xz -C targets
-  curl https://nodejs.org/dist/v10.0.0/win-x64/node.lib > targets/node-v10.0.0/node.lib
+  curl https://nodejs.org/dist/v8.11.3/node-v8.11.3-headers.tar.gz | tar xz -C targets
+  curl https://nodejs.org/dist/v8.11.3/win-x64/node.lib > targets/node-v8.11.3/node.lib
+  curl https://nodejs.org/dist/v9.11.2/node-v9.11.2-headers.tar.gz | tar xz -C targets
+  curl https://nodejs.org/dist/v9.11.2/win-x64/node.lib > targets/node-v9.11.2/node.lib
+  curl https://nodejs.org/dist/v10.8.0/node-v10.8.0-headers.tar.gz | tar xz -C targets
+  curl https://nodejs.org/dist/v10.8.0/win-x64/node.lib > targets/node-v10.8.0/node.lib
 )
 
-cl /I targets/node-v8.1.2/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node src/*.cpp targets/node-v8.1.2/node.lib
-cl /I targets/node-v9.2.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node src/*.cpp targets/node-v9.2.0/node.lib
-cl /I targets/node-v10.0.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node src/*.cpp targets/node-v10.0.0/node.lib
+cl /I targets/node-v8.11.3/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node src/*.cpp targets/node-v8.11.3/node.lib
+cl /I targets/node-v9.11.2/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node src/*.cpp targets/node-v9.11.2/node.lib
+cl /I targets/node-v10.8.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node src/*.cpp targets/node-v10.8.0/node.lib
 
 del ".\*.obj"
 del ".\dist\*.exp"
