8cb13c4f4127bfc03717e5c06d41b6cb1fc62fff
Dmitrii Goriunov <goriunovd@gmail.com>
2018-09-14 14:03:14 -0700

    Fixed make.bat installed win32 dep

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 call "%VS140COMNTOOLS%..\..\vc\vcvarsall.bat" amd64
 
-set v57=v8.1.2
-set v59=v9.2.0
+set v57=v8.12.0
+set v59=v9.11.2
 set v64=v10.10.0
 
 if not exist targets (
@@ -16,9 +16,9 @@ if not exist targets (
   curl https://nodejs.org/dist/%v64%/win-x64/node.lib > targets/node-%v64%/node.lib
 )
 
-cl /I targets/node-%v57%/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-%v57%/node.lib
-cl /I targets/node-%v59%/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-%v59%/node.lib
-cl /I targets/node-%v64%/src /I targets/node-%v64%/deps/uv/include /I targets/node-%v64%/deps/v8/include /I targets/node-%v64%/deps/openssl/openssl/include /I targets/node-%v64%/deps/zlib /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-%v64%/node.lib
+cl /I targets/node-%v57%/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node src/*.cpp targets/node-%v57%/node.lib
+cl /I targets/node-%v59%/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node src/*.cpp targets/node-%v59%/node.lib
+cl /I targets/node-%v64%/include/node /I targets/node-%v64%/deps/uv/include /I targets/node-%v64%/deps/v8/include /I targets/node-%v64%/deps/openssl/openssl/include /I targets/node-%v64%/deps/zlib /EHsc /Ox /LD /Fedist/uws_win32_64.node src/*.cpp targets/node-%v64%/node.lib
 
 del ".\*.obj"
 del ".\dist\*.exp"
