c04193fbed5c217820b68d585f1208f9708f0b47
Dmitrii Goriunov <goriunovd@gmail.com>
2018-09-30 22:36:23 +1300

    Added overwrite params

--- lib/emitter.ts
+++ lib/emitter.ts
@@ -1,8 +1,15 @@
 import { Listener } from './types';
+import { WebSocket } from './uws/client';
 
 export class EventEmitter {
   private events: { [key: string]: Listener } = {};
 
+  // need to clarify this types
+  public on(event: 'open', listener: () => void): void;
+  public on(event: 'error', listener: (err: Error) => void): void;
+  public on(event: 'message', listener: (message: string | any[]) => void): void;
+  public on(event: 'connection', listener: (socket: WebSocket) => void): void;
+  public on(event: 'disconnect', listener: (code?: number, reason?: string) => void): void;
   public on(event: string, listener: Listener): void {
     if ({}.toString.call(listener) !== '[object Function]')
       return console.log('Listener should be a function');
