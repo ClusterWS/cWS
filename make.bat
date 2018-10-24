c2ef3def6c685aad867e25a6ac81e96e8e90e5f1
Dmitrii Goriunov <goriunovd@gmail.com>
2018-10-24 19:14:56 +1300

    Add node 11 Windows bindings

--- make.bat
+++ make.bat
@@ -14,13 +14,13 @@ if not exist targets (
   curl https://nodejs.org/dist/%v64%/node-%v64%-headers.tar.gz | tar xz -C targets
   curl https://nodejs.org/dist/%v64%/win-x64/node.lib > targets/node-%v64%/node.lib
   curl https://nodejs.org/dist/%v67%/node-%v67%-headers.tar.gz | tar xz -C targets
-  curl https://nodejs.org/dist/%v67%/win-x64/node.lib > targets/node-%v67%/node.lib
+  curl https://nodejs.org/dist/%v67%/win-x67/node.lib > targets/node-%v67%/node.lib
 )
 
 cl /I targets/node-%v57%/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node src/*.cpp targets/node-%v57%/node.lib
 cl /I targets/node-%v59%/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node src/*.cpp targets/node-%v59%/node.lib
 cl /I targets/node-%v64%/include/node /I targets/node-%v64%/deps/uv/include /I targets/node-%v64%/deps/v8/include /I targets/node-%v64%/deps/openssl/openssl/include /I targets/node-%v64%/deps/zlib /EHsc /Ox /LD /Fedist/uws_win32_64.node src/*.cpp targets/node-%v64%/node.lib
-cl /I targets/node-%v67%/include/node /I targets/node-%v67%/deps/uv/include /I targets/node-%v67%/deps/v8/include /I targets/node-%v67%/deps/openssl/openssl/include /I targets/node-%v67%/deps/zlib /EHsc /Ox /LD /Fedist/uws_win32_64.node src/*.cpp targets/node-%v67%/node.lib
+cl /I targets/node-%v67%/include/node /I targets/node-%v67%/deps/uv/include /I targets/node-%v67%/deps/v8/include /I targets/node-%v67%/deps/openssl/openssl/include /I targets/node-%v67%/deps/zlib /EHsc /Ox /LD /Fedist/uws_win32_67.node src/*.cpp targets/node-%v67%/node.lib
 
 del ".\*.obj"
 del ".\dist\*.exp"
