d5038d459d58034acc4f1508b99975a3f6dacf5c
Dmitrii Goriunov <goriunovd@gmail.com>
2020-01-03 22:53:01 +1300

    Add windows support for Node 13

--- make.bat
+++ make.bat
@@ -1,10 +1,11 @@
 call "%VS140COMNTOOLS%..\..\vc\vcvarsall.bat" amd64
 
-set v57=v8.12.0
+set v57=v8.17.0
 set v59=v9.11.2
-set v64=v10.10.0
-set v67=v11.0.0
-set v72=v12.0.0
+set v64=v10.18.0
+set v67=v11.15.0
+set v72=v12.14.0
+set v79=v13.5.0
 
 if not exist targets (
   mkdir targets
@@ -18,6 +19,8 @@ if not exist targets (
   curl https://nodejs.org/dist/%v67%/win-x64/node.lib > targets/node-%v67%/node.lib
   curl https://nodejs.org/dist/%v72%/node-%v72%-headers.tar.gz | tar xz -C targets
   curl https://nodejs.org/dist/%v72%/win-x64/node.lib > targets/node-%v72%/node.lib
+  curl https://nodejs.org/dist/%v79%/node-%v79%-headers.tar.gz | tar xz -C targets
+  curl https://nodejs.org/dist/%v79%/win-x64/node.lib > targets/node-%v79%/node.lib
 )
 
 cl /I targets/node-%v57%/include/node /EHsc /Ox /LD /Fedist/cws_win32_57.node src/*.cpp targets/node-%v57%/node.lib
@@ -25,6 +28,7 @@ cl /I targets/node-%v59%/include/node /EHsc /Ox /LD /Fedist/cws_win32_59.node sr
 cl /I targets/node-%v64%/include/node /I targets/node-%v64%/deps/uv/include /I targets/node-%v64%/deps/v8/include /I targets/node-%v64%/deps/openssl/openssl/include /I targets/node-%v64%/deps/zlib /EHsc /Ox /LD /Fedist/cws_win32_64.node src/*.cpp targets/node-%v64%/node.lib
 cl /I targets/node-%v67%/include/node /I targets/node-%v67%/deps/uv/include /I targets/node-%v67%/deps/v8/include /I targets/node-%v67%/deps/openssl/openssl/include /I targets/node-%v67%/deps/zlib /EHsc /Ox /LD /Fedist/cws_win32_67.node src/*.cpp targets/node-%v67%/node.lib
 cl /I targets/node-%v72%/include/node /I targets/node-%v72%/deps/uv/include /I targets/node-%v72%/deps/v8/include /I targets/node-%v72%/deps/openssl/openssl/include /I targets/node-%v72%/deps/zlib /EHsc /Ox /LD /Fedist/cws_win32_72.node src/*.cpp targets/node-%v72%/node.lib
+cl /I targets/node-%v79%/include/node /I targets/node-%v79%/deps/uv/include /I targets/node-%v79%/deps/v8/include /I targets/node-%v79%/deps/openssl/openssl/include /I targets/node-%v79%/deps/zlib /EHsc /Ox /LD /Fedist/cws_win32_72.node src/*.cpp targets/node-%v79%/node.lib
 
 del ".\*.obj"
 del ".\dist\*.exp"
