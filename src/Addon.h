30dd09cdc841ffb5a25dfb208c97477d141fbb6a
Dmitrii Goriunov <goriunovd@gmail.com>
2020-07-04 12:14:19 +1200

    Fix base_object

--- src/Addon.h
+++ src/Addon.h
@@ -14,7 +14,7 @@
 
 #if NODE_MAJOR_VERSION==12
   #include "headers/12/tls_wrap.h"
-  #include "headers/14/base_object-inl.h"
+  #include "headers/12/base_object-inl.h"
 #endif
 
 #if NODE_MAJOR_VERSION==13
